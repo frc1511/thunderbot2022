31f967e909740f0de00dbdfef5e6a361d02d0985
plilley <plilley@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-01-20 22:51:35 +0000

    Did drive

--- trunk/thunderbot2022/src/main/cpp/Drive.cpp
+++ trunk/thunderbot2022/src/main/cpp/Drive.cpp
@@ -1,21 +1,251 @@
 #include "Drive.h"
+#include <cmath>
 
-Drive::Drive() {
+// The circumference of each wheel (meters).
+#define WHEEL_CIRCUMFERENCE 0.21
 
-}
+// The value of the encoder after 1 rotation.
+#define WHEEL_1_ROT_ENC_VAL 1
 
-Drive::~Drive() {
+// The coefficient used to convert the internal drive encoder value to meters (Drive motor).
+#define DRIVE_ENC_TO_METERS_FACTOR (WHEEL_CIRCUMFERENCE / WHEEL_1_ROT_ENC_VAL)
 
-}
+// The coeffieient used to convert a radian value to internal encoder value (Turning motor).
+#define ROT_RAD_TO_ENC_FACTOR 10.1859
 
-void Drive::process() {
+#define DRIVE_MAX_VOLTAGE 12
+#define TURNING_MAX_VOLTAGE 12
 
-}
+// --- PID values ---
 
-SwerveModule::SwerveModule() {
+#define DRIVE_P_VALUE 1
+#define DRIVE_I_VALUE 0
+#define DRIVE_D_VALUE 0
+#define DRIVE_I_ZONE_VALUE 0
+#define DRIVE_FF_VALUE 0
 
+#define ROT_P_VALUE 0.4
+#define ROT_I_VALUE 0
+#define ROT_D_VALUE 0
+#define ROT_I_ZONE_VALUE 0
+#define ROT_FF_VALUE 0
+
+// --- Swerve module ---
+
+SwerveModule::SwerveModule(int driveCANID, int turningCANID, int canCoderCANID, double canCoderOffset)
+  : driveMotor(driveCANID, rev::CANSparkMax::MotorType::kBrushless),
+    driveEncoder(driveMotor.GetEncoder()),
+    drivePID(driveMotor.GetPIDController()),
+    turningMotor(turningCANID, rev::CANSparkMax::MotorType::kBrushless),
+    turningRelEncoder(turningMotor.GetEncoder()),
+    turningPID(turningMotor.GetPIDController()),
+    turningAbsEncoder(canCoderCANID),
+    canCoderOffset(canCoderOffset) {
+  
+    // --- Drive motor config ---
+    
+    driveMotor.RestoreFactoryDefaults();
+    // Brake when idle.
+    driveMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+    driveMotor.EnableVoltageCompensation(DRIVE_MAX_VOLTAGE);
+    // Limit in amps (Always when using NEO Brushless to avoid damage).
+    driveMotor.SetSmartCurrentLimit(40);
+    driveMotor.SetInverted(false);
+    // Ramping (0.5 seconds to accelerate from neutral to full throttle).
+    driveMotor.SetClosedLoopRampRate(0.5);
+    driveMotor.SetOpenLoopRampRate(0.5);
+    // Frame period.
+    driveMotor.SetPeriodicFramePeriod(rev::CANSparkMaxLowLevel::PeriodicFrame::kStatus0, 10);
+    driveMotor.SetPeriodicFramePeriod(rev::CANSparkMaxLowLevel::PeriodicFrame::kStatus1, 10);
+    driveMotor.SetPeriodicFramePeriod(rev::CANSparkMaxLowLevel::PeriodicFrame::kStatus2, 10);
+    // Apply a coefficient to convert the encoder value to meters.
+    driveEncoder.SetPositionConversionFactor(DRIVE_ENC_TO_METERS_FACTOR);
+    // PID Values.
+    drivePID.SetP(DRIVE_P_VALUE, 0);
+    drivePID.SetI(DRIVE_I_VALUE, 0);
+    drivePID.SetD(DRIVE_D_VALUE, 0);
+    drivePID.SetIZone(DRIVE_I_ZONE_VALUE, 0);
+    drivePID.SetFF(DRIVE_FF_VALUE, 0);
+  
+    // --- Turning motor config ---
+    
+    turningMotor.RestoreFactoryDefaults();
+    turningMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    turningMotor.EnableVoltageCompensation(TURNING_MAX_VOLTAGE);
+    turningMotor.SetSmartCurrentLimit(30);
+    turningMotor.SetInverted(true);
+    turningPID.SetFeedbackDevice(turningRelEncoder);
+    // PID values.
+    turningPID.SetP(ROT_P_VALUE, 0);
+    turningPID.SetI(ROT_I_VALUE, 0);
+    turningPID.SetD(ROT_D_VALUE, 0);
+    turningPID.SetIZone(ROT_I_ZONE_VALUE, 0);
+    turningPID.SetFF(ROT_FF_VALUE, 0);
+    
+    // --- CANCoder config ---
+    
+    turningAbsEncoder.ConfigAbsoluteSensorRange(ctre::phoenix::sensors::AbsoluteSensorRange::Signed_PlusMinus180);
 }
 
 SwerveModule::~SwerveModule() {
 
+}
+
+void SwerveModule::setState(frc::SwerveModuleState targetState) {
+    frc::SwerveModuleState currentState = getState();
+  
+    // Optimize the target state by flipping motor directions and adjusting rotations in order to turn the least amount of distance possible.
+    frc::SwerveModuleState optimizedState = frc::SwerveModuleState::Optimize(targetState, currentState.angle);
+    
+    // Only handle turning when we are actually driving.
+    if(units::math::abs(optimizedState.speed) > 0.01_mps) {
+        // Rotate the swerve module.
+        setTurningMotor(optimizedState.angle.Radians());
+    }
+  
+    // Set the drive motor's velocity.
+    setDriveMotor(optimizedState.speed.value());
+}
+
+frc::SwerveModuleState SwerveModule::getState() {
+    return { units::meters_per_second_t(getDriveVelocity()), frc::Rotation2d(getAbsoluteRotation()) };
+}
+
+void SwerveModule::setDriveMotor(double speed) {
+    driveMotor.Set(speed);
+}
+
+double SwerveModule::getDriveVelocity() {
+    return driveEncoder.GetVelocity();
+}
+
+void SwerveModule::setTurningMotor(units::radian_t angle) {
+    // Subtract the absolute rotation from the target rotation.
+    units::radian_t rotation(angle - getAbsoluteRotation());
+    
+    // Fix the discontinuity problem.
+    // If the value is above π rad or below -π rad...
+    if(units::math::abs(rotation).value() > wpi::numbers::pi) {
+        // Subtract 2π rad, or add 2π rad depending on the sign.
+        rotation = units::radian_t(rotation.value() - (2 * wpi::numbers::pi) * (std::signbit(rotation.value()) ? -1 : 1));
+    }
+    
+    // Convert the radian value to internal encoder value.
+    double output = rotation.value() * ROT_RAD_TO_ENC_FACTOR;
+    
+    // Add the current relative rotation.
+    output += getRelativeRotation();
+    
+    // Set PID controller reference.
+    // turningPID.SetReference(output, rev::ControlType::kPosition);
+    turningPID.SetReference(output, rev::CANSparkMax::ControlType::kPosition);
+}
+
+double SwerveModule::getRelativeRotation() {
+    return turningRelEncoder.GetPosition();
+}
+
+units::radian_t SwerveModule::getAbsoluteRotation() {
+    return units::radian_t(units::degree_t(turningAbsEncoder.GetAbsolutePosition())) - canCoderOffset;
+}
+
+// --- Drivetrain ---
+
+Drive::Drive() {
+    resetIMU();
+    // Configure the calibration time to 4 seconds.
+    imu.ConfigCalTime(frc::ADIS16470_IMU::CalibrationTime::_4s);
+    // Set axis for the gryo to take (Z is up and down).
+    imu.SetYawAxis(frc::ADIS16470_IMU::IMUAxis::kZ);
+    // Calibrate the IMU.
+    calibrateIMU();
+}
+
+Drive::~Drive() {
+    for (SwerveModule* module : swerveModules) {
+        delete module;
+    }
+}
+
+void Drive::process() {
+    updateOdometry();
+}
+
+frc::Rotation2d Drive::getRotation() {
+    double rotation = fmod(imu.GetAngle().value(), 360);
+
+    // Convert -360 to 360 value into -180 to 180 value.
+    if(abs(rotation) > 180) {
+        rotation -= (360 * (std::signbit(rotation) ? -1 : 1));
+    }
+    
+    return frc::Rotation2d(units::degree_t(rotation));
+}
+
+void Drive::resetForward() {
+    resetIMU();
+}
+
+void Drive::calibrateIMU() {
+    imu.Calibrate();
+}
+
+void Drive::setVelocities(double xVel, double yVel, double rotVel, bool fieldCentric) {
+    if (fieldCentric) {
+        setModuleStates(frc::ChassisSpeeds::FromFieldRelativeSpeeds(units::meters_per_second_t(xVel), units::meters_per_second_t(yVel), units::radians_per_second_t(rotVel), getRotation()));
+    } else {
+        setModuleStates({ units::meters_per_second_t(xVel), units::meters_per_second_t(yVel), units::radians_per_second_t(rotVel) });
+    }
+}
+
+void Drive::cmdRotate(units::radian_t anlge) {
+
+}
+
+void Drive::cmdDrive(units::meter_t x, units::meter_t y, units::radian_t angle) {
+
+}
+
+bool Drive::cmdIsFinished() {
+
+    return true;
+}
+
+void Drive::cmdCancel() {
+
+}
+
+void Drive::setModuleStates(frc::ChassisSpeeds chassisSpeeds) {
+    // Generate module states using chassis velocities. The magic function of swerve drive.
+    wpi::array<frc::SwerveModuleState, 4> moduleStates = kinematics.ToSwerveModuleStates(chassisSpeeds);
+    
+    // Recalculate wheel speeds relative to the max speed.
+    kinematics.DesaturateWheelSpeeds(&moduleStates, MAX_SPEED);
+    
+    // Set the module states.
+    for(unsigned i = 0; i < swerveModules.size(); i++) {
+      swerveModules.at(i)->setState(moduleStates.at(i));
+    }
+}
+
+void Drive::updateOdometry() {
+    // Update the position and rotation on the field.
+    odometry.Update(getRotation(),
+        swerveModules.at(0)->getState(),
+        swerveModules.at(1)->getState(),
+        swerveModules.at(2)->getState(),
+        swerveModules.at(3)->getState());
+}
+
+void Drive::resetOdometry(frc::Pose2d pose) {
+    // Reset the position on the field.
+    odometry.ResetPosition(pose, getRotation());
+}
+
+frc::Pose2d Drive::getPose() {
+    return odometry.GetPose();
+}
+
+void Drive::resetIMU() {
+    imu.Reset();
 }
--- trunk/thunderbot2022/src/main/include/Drive.h
+++ trunk/thunderbot2022/src/main/include/Drive.h
@@ -1,23 +1,189 @@
 #pragma once
 
-class SwerveModule;
+#include "IOMap.h"
+#include <frc/geometry/Transform2d.h>
+#include <frc/geometry/Rotation2d.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/kinematics/SwerveDriveOdometry.h>
+#include <frc/kinematics/SwerveModuleState.h>
+#include <frc/kinematics/ChassisSpeeds.h>
+#include <frc/ADIS16470_IMU.h>
+#include <rev/CANSparkMax.h>
+#include <ctre/phoenix/sensors/CANCoder.h>
+#include <wpi/array.h>
+#include <wpi/numbers>
 
+#define MAX_SPEED 4_mps
+#define AUTO_SPEED 1_mps
+
+#define ROBOT_WIDTH 0.54_m
+#define ROBOT_LENGTH 0.67_m
+
+// --- Swerve module ---
+
+/**
+ * Represents a singular swerve module on the robot.
+ */
+class SwerveModule {
+public:
+    SwerveModule(int driveCANID, int turningCANID, int canCoderCANID, double canCoderOffset);
+    ~SwerveModule();
+
+    /**
+     * Sets the state of the swerve module.
+     */
+    void setState(frc::SwerveModuleState state);
+
+    /**
+     * Returns the current state of the swerve module.
+     */
+    frc::SwerveModuleState getState();
+
+private:
+    /**
+     * Sets the speed of the drive motor (-1 to 1).
+     */
+    void setDriveMotor(double speed);
+
+    /**
+     * Returns the current velocity (RPM) of the drive motor.
+     */
+    double getDriveVelocity();
+
+    /**
+     * Sets the angle of the swerve module.
+     */
+    void setTurningMotor(units::radian_t angle);
+
+    /**
+     * Returns the relative rotation of the module (NEO 550 internal encoder).
+     */
+    double getRelativeRotation();
+
+    /**
+     * Returns the absolute rotation of the module (CANCoder).
+     */ 
+    units::radian_t getAbsoluteRotation();
+
+    // The drive motor (NEO Brushless motor).
+    rev::CANSparkMax driveMotor;
+    rev::SparkMaxRelativeEncoder driveEncoder;
+    rev::SparkMaxPIDController drivePID;
+
+    // The turning motor (NEO 550).
+    rev::CANSparkMax turningMotor;
+    rev::SparkMaxRelativeEncoder turningRelEncoder;
+    rev::SparkMaxPIDController turningPID;
+
+    // The absolute encoder (CTRE CANCoder).
+    ctre::phoenix::sensors::CANCoder turningAbsEncoder;
+
+    // The offset of the CANCoder.
+    const units::radian_t canCoderOffset;
+};
+
+// --- Drivetrain ---
+
+/**
+ * Represents the drivetrain of the robot and handles all drive-related functionality.
+ */
 class Drive {
 public:
     Drive();
     ~Drive();
 
     void process();
+    
+    /**
+     * Returns the rotation of the robot.
+     */
+    frc::Rotation2d getRotation();
+
+    /**
+     * Resets the rotation of the robot (field-centric).
+     */
+    void resetForward();
+
+    /**
+     * Calibrates the IMU.
+     */
+    void calibrateIMU();
+
+    /**
+     * Sets the velocities of the robot.
+     */
+    void setVelocities(double xVel, double yVel, double rotVel, bool fieldCentric = true);
+    
+    /**
+     * Begins a command to rotate a specified angle.
+     */
+    void cmdRotate(units::radian_t anlge);
+
+    /**
+     * Begins a command to drive a specified distance.
+     */
+    void cmdDrive(units::meter_t x, units::meter_t y, units::radian_t angle = units::radian_t(0));
+    
+    /**
+     * Returns whether the last command has finished.
+     */
+    bool cmdIsFinished();
+    
+    /**
+     * Cancels the current command.
+     */
+    void cmdCancel();
 
 private:
-    // Something here...
-};
+    /**
+     * Generates and sends states to the swerve modules from chassis speeds.
+     */
+    void setModuleStates(frc::ChassisSpeeds chassisSpeeds);
 
-class SwerveModule {
-public:
-    SwerveModule();
-    ~SwerveModule();
+    /**
+     * Updates the position and rotation on the field.
+     */
+    void updateOdometry();
 
-private:
-    // Something here...
+    /**
+     * Resets the position and rotation on the field.
+     */
+    void resetOdometry(frc::Pose2d pose = frc::Pose2d());
+
+    /**
+     * Returns the current pose.
+     */
+    frc::Pose2d getPose();
+
+    /**
+     * Resets the IMU to 0.
+     */
+    void resetIMU();
+
+    // The locations of the swerve modules on the robot.
+    wpi::array<frc::Translation2d, 4> locations {
+        frc::Translation2d(-ROBOT_WIDTH/2, +ROBOT_LENGTH/2), // Front left.
+        frc::Translation2d(-ROBOT_WIDTH/2, -ROBOT_LENGTH/2), // Back left.
+        frc::Translation2d(+ROBOT_WIDTH/2, -ROBOT_LENGTH/2), // Back right.
+        frc::Translation2d(+ROBOT_WIDTH/2, +ROBOT_LENGTH/2), // Front right.
+    };
+    
+    // The swerve modules on the robot.
+    wpi::array<SwerveModule*, 4> swerveModules {
+      new SwerveModule(CAN_SWERVE_FL_DRIVE_MOTOR, CAN_SWERVE_FL_ROT_MOTOR, CAN_SWERVE_FL_ROT_CAN_CODER, +0),
+      new SwerveModule(CAN_SWERVE_BL_DRIVE_MOTOR, CAN_SWERVE_BL_ROT_MOTOR, CAN_SWERVE_BL_ROT_CAN_CODER, +0),
+      new SwerveModule(CAN_SWERVE_BR_DRIVE_MOTOR, CAN_SWERVE_BR_ROT_MOTOR, CAN_SWERVE_BR_ROT_CAN_CODER, +0),
+      new SwerveModule(CAN_SWERVE_FR_DRIVE_MOTOR, CAN_SWERVE_FR_ROT_MOTOR, CAN_SWERVE_FR_ROT_CAN_CODER, +0),
+    };
+  
+    // The helper class that converts chassis speeds into swerve module states.
+    frc::SwerveDriveKinematics<4> kinematics { locations };
+    
+    // The odometry class that tracks the position of the robot on the field.
+    frc::SwerveDriveOdometry<4> odometry { kinematics, getRotation() };
+    
+    // The ADIS16470 IMU (3D gyro and accelerometer).
+
+    frc::ADIS16470_IMU imu {};
 };
