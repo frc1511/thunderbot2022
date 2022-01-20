d2e4fb93eb23821ddf8d016e042adc4f5f1ca6f5
Peter Lilley <hi.petelilley@gmail.com>
2022-01-20 00:21:30 +0000

    Updated controls constructor

--- src/main/cpp/Controls.cpp
+++ src/main/cpp/Controls.cpp
@@ -1,6 +1,7 @@
 #include "Controls.h"
 
-Controls::Controls(Drive* drive) : drive(drive) {
+Controls::Controls(Drive* drive, GamEpiece* gamEpiece, Hang* hang, Limelight* limelight) 
+: drive(drive), gamEpiece(gamEpiece), hang(hang), limelight(limelight) {
 
 }
 
--- src/main/include/Controls.h
+++ src/main/include/Controls.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include "Drive.h"
+#include "GamEpiece.h"
+#include "Hang.h"
+#include "Limelight.h"
 
 class Controls {
 public:
-    Controls(Drive* drive);
+    Controls(Drive* drive, GamEpiece* gamEpiece, Hang* hang, Limelight* limelight);
     ~Controls();
 
     void process();
 
 private:
     Drive* drive;
+    GamEpiece* gamEpiece;
+    Hang* hang;
+    Limelight* limelight;
 
     // Something here...
 };
