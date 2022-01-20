44c51fdde9ca2a9c39c02f83f350f5520c42b336
ishan <ishan@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-01-20 02:02:58 +0000

    started hang

--- trunk/thunderbot2022/src/main/cpp/Hang.cpp
+++ trunk/thunderbot2022/src/main/cpp/Hang.cpp
@@ -1,13 +1,64 @@
 #include "Hang.h"
+#include "string"
 
 Hang::Hang() {
-
+reset();
 }
 
-Hang::~Hang() {
-
-}
+Hang::~Hang() {}
 
 void Hang::process() {
+    switch(hangState)
+    {
+            case MID:
+                break;
+            case HIGH:
+                break;
+            case TRAVERSAL:
+                break;
+            case NOT_ON_BAR:
+                break;
+    }
+
+}
+
+void Hang::reset() {
+    int step = 0;
+    bool isOnBar = false;
+}
+
+void Hang::debug() {
+    std::string stateString = "";
+    switch(hangState)
+    {
+        case MID:
+            stateString = "going to mid";
+            break;
+        case HIGH:
+            stateString = "going to high";
+            break;
+        case TRAVERSAL:
+            stateString = "going to traversal";
+            break;
+    }
+}
+
+void Hang::pivot() {
+    //toggles hang pivot piston
+    hangPivot.Toggle();
+}
+
+void Hang::retract() {
+
+}
+
+void Hang::reversePivot() {
+//dont know how to do this yet, maybe slow exhaust. Check on Hang subteam later
+}
+
+void Hang::engageBrake() {
+}
+
+void Hang::disengageBrake() {
 
 }
--- trunk/thunderbot2022/src/main/include/Hang.h
+++ trunk/thunderbot2022/src/main/include/Hang.h
@@ -1,12 +1,85 @@
 #pragma once
+#include "IOMap.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
+#include "rev/CANSparkMax.h"
+#pragma GCC diagnostic pop
+#include <frc/DoubleSolenoid.h>
+
+
+/*
+PART 1
+    -ACTUATORS
+        -NEO
+        -pivot piston
+        -brake piston
+
+    -SENSORS
+        -optical sensor at bottom inside hang to check if fully retracted
+        -flag sensor(2x - one on each arm) to check if retracted
+        -encoder on winch to get exact axle measurement
+
+    -DRIVER FEEDBACK
+        -which bar robot is on/robot moving between bars
+            -on dashboard
+        -whether the robot is moving between bars or on the bar safely
+
+PART 2
+    COMMANDS:
+        MID BAR:
+            -disengage brake-brake piston
+            -once fully extended, retract until fully retracted - optical sensor to know when retracted,
+                flag sensors to know if extended
+            -engage brake - brake piston
+        HIGH/TRAVERSAL BAR:
+            -disengage brake-brake piston
+            -slightly extend arm-encoder
+            -engage brake-brake piston
+            -pivot arm all the way-pivot piston
+            -disengage brake-brake piston
+            -pivot a tiny bit to hit high bar-dont know yet-probably slow exhaust?
+            -retract until fully retracted- know if extended with flag sensor, 
+                know if retracted with optical sensor
+            -engage brake-brake piston
+*/
 
 class Hang {
 public:
     Hang();
     ~Hang();
 
+    //functions
+    //does the important stuff
     void process();
+    //sends functions to dashboard
+    void debug();
+    //resets variables
+    void reset();
+    //pivots hang
+    void pivot();
+    //pivots the arms backwards
+    void reversePivot();
+    //engages brake to stop arms
+    void engageBrake();
+    //disengages brake to release arms
+    void disengageBrake();
+    //retracts arms
+    void retract();
+
+    //bar that the robot is going to
+    enum HangState{TRAVERSAL, HIGH, MID, NOT_ON_BAR};
+    //enumerator variable thing
+    HangState hangState;
 
 private:
-    // Something here...
+    //step that the robot is on in the motion to traversal;
+    int step = 0;
+    //whether the robot is on the bar
+    bool isOnBar = false;
+
+    //actuator stuff
+    rev::CANSparkMax winchMotor{CAN_HANG_WINCH_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    frc::DoubleSolenoid hangPivot{frc::PneumaticsModuleType::CTREPCM, INTAKE_PIVOT_EXTEND, INTAKE_PIVOT_RETRACT};
+    frc::DoubleSolenoid brake{frc::PneumaticsModuleType::CTREPCM, PCM1_HANG_LEFT_BRAKE_PISTON_EXTEND, PCM1_HANG_LEFT_BRAKE_PISTON_RETRACT};
+
 };
