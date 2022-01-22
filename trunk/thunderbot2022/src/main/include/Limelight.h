b6f46f2bcd67aea26f0624b930eec0b9c9d5c74b
plilley <plilley@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-01-22 17:18:29 +0000

    Implemented limelight class

--- trunk/thunderbot2022/src/main/include/Limelight.h
+++ trunk/thunderbot2022/src/main/include/Limelight.h
@@ -1,12 +1,67 @@
 #pragma once
 
+#include <frc/smartdashboard/Smartdashboard.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+#include <units/math.h>
+#include <memory>
+
+/**
+ * Represents the limelight sensor on the robot. :D
+ */
 class Limelight {
 public:
     Limelight();
     ~Limelight();
 
-    void process();
+    /**
+     * Returns whether the limelight has any valid targets.
+     */
+    bool hasTarget();
+
+    /**
+     * Returns the horizontal angle to the target.
+     */
+    units::radian_t getAngleHorizontal();
+
+    /**
+     * Returns the vertical angle to the target.
+     */
+    units::radian_t getAngleVertical();
+
+    enum class LEDMode {
+        DEFAULT = 0,
+        OFF     = 1,
+        BLINK   = 2,
+        ON      = 3,
+    };
+
+    /**
+     * Sets the mode of the limelight LEDs.
+     */
+    void setLEDMode(LEDMode mode);
+
+    /**
+     * Returns the current LED mode.
+     */
+    LEDMode getLEDMode();
+
+    enum class CameraMode {
+        VISION_PROCESS = 0,
+        DRIVER_CAMERA  = 1,
+    };
+
+    /**
+     * Sets the mode of the limelight camera.
+     */
+    void setCameraMode(CameraMode mode);
+
+    /**
+     * Returns the current camera mode.
+     */
+    CameraMode getCameraMode();
 
 private:
-    // Something here...
+    // A network table used to communicate with limelight.
+    std::shared_ptr<nt::NetworkTable> table;
 };
