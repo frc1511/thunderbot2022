ecb878bdfcc05d56ac4eed8fac19920a5e955428
plilley <plilley@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-01-23 17:51:15 +0000

    Worked on implementing ball detection using OpenCV

---
+++ trunk/thunderbot2022/src/main/cpp/Camera.cpp
@@ -0,0 +1,126 @@
+#include "Camera.h"
+
+#define CAMERA_WIDTH 320
+#define CAMERA_HEIGHT 240
+#define CAMERA_FPS 30
+#define CAMERA_EXPOSURE 40
+
+// TODO Determine low and high thresholds for red and blue balls.
+
+const cv::Scalar CAMERA_RED_LOW(0, 0, 0);
+const cv::Scalar CAMERA_RED_HIGH(0, 0, 0);
+const cv::Scalar CAMERA_BLUE_LOW(0, 0, 0);
+const cv::Scalar CAMERA_BLUE_HIGH(0, 0, 0);
+
+Camera::Camera()
+  : camera(frc::CameraServer::StartAutomaticCapture()) {
+
+    camera.SetResolution(CAMERA_WIDTH, CAMERA_HEIGHT);
+    camera.SetFPS(CAMERA_FPS);
+    camera.SetExposureManual(CAMERA_EXPOSURE);
+
+    cvSink = frc::CameraServer::GetVideo();
+
+    outputStream = frc::CameraServer::PutVideo("intake_camera", CAMERA_WIDTH, CAMERA_HEIGHT);
+}
+
+Camera::~Camera() {
+    //hi trevor :D
+}
+
+void Camera::process() {
+    static Frame frame {};
+    
+    getFrame(&frame);
+    sendFrame(frame);
+}
+
+void Camera::getFrame(Frame* frame) {
+    // Grab a frame from the camera and encode it into the image matrix.
+    // Notify the output if there was an error getting the frame.
+    if (!cvSink.GrabFrame(frame->matrix)) {
+        outputStream.NotifyError(cvSink.GetError());
+        return;
+    }
+}
+
+void Camera::sendFrame(Frame& frame) {
+    // Send the frame to the driver station.
+    outputStream.PutFrame(frame.matrix);
+}
+
+std::optional<Camera::FrameSector> Camera::locateTarget(Frame& frame) {
+    cv::Mat targetMatrix;
+
+    // Convert colorspace to HSV.
+    cv::cvtColor(frame.matrix, targetMatrix, cv::COLOR_BGR2HSV);
+    
+    // Apply threshold to matrix (depending on alliance color).
+    switch (frc::DriverStation::GetAlliance()) {
+        case frc::DriverStation::kRed:
+            cv::inRange(targetMatrix, CAMERA_RED_LOW, CAMERA_RED_HIGH, targetMatrix);
+            break;
+        case frc::DriverStation::kBlue:
+            cv::inRange(targetMatrix, CAMERA_BLUE_LOW, CAMERA_BLUE_HIGH, targetMatrix);
+            break;
+        case frc::DriverStation::kInvalid:
+            return {};
+    }
+    
+    // Blur image.
+    cv::medianBlur(targetMatrix, targetMatrix, 3);
+    
+    // Convert colorspace to BGR.
+    cv::cvtColor(targetMatrix, targetMatrix, cv::COLOR_GRAY2BGR);
+    
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    
+    // Get the contours of the target.
+    cv::findContours(targetMatrix, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point());
+
+    const size_t contourNum = contours.size();
+    
+    if (contourNum == 0) {
+        return {};
+    }
+    
+    // Draw contours on image.
+    for (size_t i = 0; i < contourNum; ++i) {
+        cv::Scalar contourColor = cv::Scalar(255, 0, 0);
+        cv::drawContours(targetMatrix, contours, (int)i, contourColor, 2, 8, hierarchy, 0, cv::Point());
+    }
+    
+    size_t largestArea = 0;
+    std::vector<cv::Point>* targetContours = nullptr;
+    
+    // Find the largest mass.
+    for (size_t i = 0; i < contourNum; ++i) {
+        size_t area = cv::contourArea(contours[i]);
+        if (area > largestArea) {
+            largestArea = area;
+            targetContours = &contours[i];
+        }
+    }
+
+    // TODO Check to make sure area is above a certain value.
+
+    // Get moments.
+    cv::Moments moments = cv::moments(*targetContours, false);
+    
+    // Get the center.
+    cv::Point2f center = cv::Point2f(moments.m10/moments.m00, moments.m01/moments.m00);
+    
+    const size_t sectorSize = CAMERA_WIDTH / 3;
+    
+    // Get sector of the frame.
+    if (center.x < sectorSize) {
+        return LEFT;
+    }
+    else if (center.x > sectorSize * 2) {
+        return RIGHT;
+    }
+    else {
+        return CENTER;
+    }
+}
---
+++ trunk/thunderbot2022/src/main/include/Camera.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <Mechanism.h>
+#include <cameraserver/CameraServer.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <frc/DriverStation.h>
+#include <units/math.h>
+#include <optional>
+#include <vector>
+
+/**
+ * Represents a USB Camera and handles vision processing.
+ */
+class Camera : public Mechanism {
+public:
+    Camera();
+    ~Camera();
+
+    void process() override;
+
+    struct Frame {
+        // Matrix used by OpenCV to encode images into.
+        cv::Mat matrix {};
+    };
+
+    /**
+     * Gets the current frame of the camera.
+     */
+    void getFrame(Frame* frame);
+
+    /**
+     * Sends a frame to the driver station.
+     */
+    void sendFrame(Frame& frame);
+    
+    // The region of the frame.
+    enum FrameSector {
+        LEFT,
+        CENTER,
+        RIGHT,
+    };
+    
+    /**
+     * Attempts to locate a cargo in front of the robot and returns the sector the cargo is in.
+     */
+    std::optional<FrameSector> locateTarget(Frame& frame);
+    
+private:
+    // The USB Camera.
+    cs::UsbCamera camera;
+    
+    // CvSink that is used to capture matricies from the camera.
+    cs::CvSink cvSink;
+    
+    // The image stream used to send images back to dashboard.
+    cs::CvSource outputStream;
+};
