7c1180f5c0f3ade2b9754e9b83f2bf495a0dfd08
celd565 <celd565@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-01-26 00:16:17 +0000

    intake code

--- trunk/thunderbot2022/src/main/include/Intake.h
+++ trunk/thunderbot2022/src/main/include/Intake.h
@@ -2,29 +2,46 @@
 
 #include "Mechanism.h"
 #include "Feedback.h"
+#include "IOMap.h"
+#include <frc/DoubleSolenoid.h>
+#include "rev/CANSparkMax.h"
+#include <frc/DigitalInput.h>
+
 /** has two sensors and two motors
  * stage one banner sensor and stage two banner sensor
  * and two double solonoids
  */
-class Intake : public Mechanism {
+class Intake : public Mechanism
+{
 public:
     Intake();
     ~Intake();
-
     void resetToMode(MatchMode mode) override;
     void sendFeedback() override;
     void process() override;
 
-    enum IntakeDirection{
+    enum IntakeDirection
+    {
         INTAKE,
         OUTTAKE,
         NOTTAKE,
         FORCE_STAGE_TWO
     };
+
     void setIntakeDirection(IntakeDirection intakeDirection);
     void setIntakePosition(bool position); // true is down, false is up
-    void setIntakeSpeed(double speed); // positive to intake, negative to outtake, remember to make sure thing is down before you spin it :D
+    void setIntakeSpeed(double speed);     // positive to intake, negative to outtake, intake must be down to work correctly
+
 private:
-    
-        // Something here...
+    // Something here...
+    frc::DoubleSolenoid rightIntake{frc::PneumaticsModuleType::CTREPCM, INTAKE_RIGHT_PIVOT_EXTEND, INTAKE_RIGHT_PIVOT_RETRACT};
+    frc::DoubleSolenoid leftIntake{frc::PneumaticsModuleType::CTREPCM, INTAKE_LEFT_PIVOT_EXTEND, INTAKE_LEFT_PIVOT_RETRACT};
+    rev::CANSparkMax intakeMotorStageOne{CAN_INTAKE_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax intakeMotorStageTwo{CAN_INTAKE_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    bool intakeposition;
+    double intakespeed;
+    IntakeDirection targetdirection;
+    frc::DigitalInput stageOneFlag {DIO_STORAGE_BANNER_STAGE_ONE}; //true is present, false is not present
+    frc::DigitalInput stageTwoFlag {DIO_STORAGE_BANNER_STAGE_TWO}; //true is present, false is not present
+
 };
