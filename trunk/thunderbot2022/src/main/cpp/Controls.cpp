cdcce39db83ff52499c13495f762c2fa9d355bea
plilley <plilley@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-02-06 00:25:47 +0000

    Fixed controls

--- trunk/thunderbot2022/src/main/cpp/Controls.cpp
+++ trunk/thunderbot2022/src/main/cpp/Controls.cpp
@@ -1,21 +1,21 @@
 #include "Controls.h"
 
-#define WATER_BUG_BUTTON      2 // GetRawButton() give bool
-#define ALARM_CLOCK_BUTTON      3 // GetRawButton() give bool
-#define T_STICK_BUTTON      1 // GetRawButton() give bool
-#define SWING_SET_BUTTON      4 // GetRawButton() give bool
-#define LEFT_BUMPER   5 // GetRawButton() give bool
-#define RIGHT_BUMPER  6 // GetRawButton() give bool
-#define BACK_BUTTON   9 // GetRawButton() give bool
-#define START_BUTTON  10 // GetRawButton() give bool
-#define LEFT_TRIGGER  3 // GetRawAxis() give double
-#define RIGHT_TRIGGER 4 // GetRawAxis() give double
-#define LEFT_TRIGGER_BUTTON 7  // GetRawButton() give bool
+#define WATER_BUG_BUTTON     2  // GetRawButton() give bool
+#define ALARM_CLOCK_BUTTON   3  // GetRawButton() give bool
+#define T_STICK_BUTTON       1  // GetRawButton() give bool
+#define SWING_SET_BUTTON     4  // GetRawButton() give bool
+#define LEFT_BUMPER          5  // GetRawButton() give bool
+#define RIGHT_BUMPER         6  // GetRawButton() give bool
+#define BACK_BUTTON          9  // GetRawButton() give bool
+#define START_BUTTON         10 // GetRawButton() give bool
+#define LEFT_TRIGGER         3  // GetRawAxis() give double
+#define RIGHT_TRIGGER        4  // GetRawAxis() give double
+#define LEFT_TRIGGER_BUTTON  7  // GetRawButton() give bool
 #define RIGHT_TRIGGER_BUTTON 8  // GetRawButton() give bool
-#define LEFT_STICK_PRESS 11 // GetRawButton() give bool
-#define RIGHT_STICK_PRESS 12 // GetRawButton() give bool
-#define HOME_BUTTON 13 // GetRawButton() give bool
-#define BIG_MIDDLE_BUTTON 14 // GetRawButton() give bool
+#define LEFT_STICK_PRESS     11 // GetRawButton() give bool
+#define RIGHT_STICK_PRESS    12 // GetRawButton() give bool
+#define HOME_BUTTON          13 // GetRawButton() give bool
+#define BIG_MIDDLE_BUTTON    14 // GetRawButton() give bool
 
 #define LEFT_X_AXIS   0 // GetRawAxis() give double
 #define LEFT_Y_AXIS   1 // GetRawAxis() give double
@@ -23,7 +23,7 @@
 #define RIGHT_Y_AXIS  5 // GetRawAxis() give double
 #define DPAD          0 // GetPOV() give int
 
-#define AXIS_DEADZONE .25
+#define AXIS_DEADZONE .05
 
 #ifdef HOMER
 Controls::Controls(Drive* drive) : drive(drive) {
@@ -68,18 +68,21 @@ void Controls::process() {
     bool configOffsets           = false; // Don't implement because 
     double xAxisVelocity         = controllerDriver.GetRawAxis(LEFT_X_AXIS);
     double yAxisVelocity         = controllerDriver.GetRawAxis(LEFT_Y_AXIS);
-    double leftRotationVelocity  = controllerDriver.GetRawAxis(LEFT_TRIGGER);
-    double rightRotationVelocity = controllerDriver.GetRawAxis(RIGHT_TRIGGER);
+    double leftRotationVelocity  = (controllerDriver.GetRawAxis(LEFT_TRIGGER)+1)/2;
+    double rightRotationVelocity = (controllerDriver.GetRawAxis(RIGHT_TRIGGER)+1)/2;
     int slowDriveDirection       = controllerDriver.GetPOV(DPAD);
     bool slowLeftVelocity        = controllerDriver.GetRawButton(LEFT_BUMPER);
     bool slowRightVelocity       = controllerDriver.GetRawButton(RIGHT_BUMPER);
     bool toggleSlowMode          = controllerDriver.GetRawButton(ALARM_CLOCK_BUTTON);
+
+    // std::cout << xAxisVelocity << ", " << yAxisVelocity << '\n';
 
     bool disableDrive = false;
 
     if(resetPosition) {
         drive->zeroRotation();
     }
+
 
     if(toggleDriveMode) {
         if(!wasDriveModeToggled) {
@@ -105,25 +108,26 @@ void Controls::process() {
         drive->configMagneticEncoders();
     }
 
-    double finalXAxis = 0.0;
+    double finalXAxis = 0.0000000000;
     double finalYAxis = 0.0;
     double finalRotation = 0.0;
 
     // DPad not pressed, so normal mode.
     if(slowDriveDirection == -1) {
         if(slowModeEnabled){
-            if(abs(xAxisVelocity) > AXIS_DEADZONE) {
+            if(fabs(xAxisVelocity) > AXIS_DEADZONE) {
                 finalXAxis = (.2 * xAxisVelocity);
             }
-            if(abs(yAxisVelocity) > AXIS_DEADZONE) {
+            if(fabs(yAxisVelocity) > AXIS_DEADZONE) {
                 finalYAxis = (.2 * yAxisVelocity);
             }
         }
         else{
-            if(abs(xAxisVelocity) > AXIS_DEADZONE) {
+            if(fabs(xAxisVelocity) > AXIS_DEADZONE) {
                 finalXAxis = xAxisVelocity;
+                std::cout << finalXAxis << ", " << xAxisVelocity << "\n";
             }
-            if(abs(yAxisVelocity) > AXIS_DEADZONE) {
+            if(fabs(yAxisVelocity) > AXIS_DEADZONE) {
                 finalYAxis = yAxisVelocity;
             }
         }
