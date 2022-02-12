e840d91b36bc88526b9d2ee7ba7bc24d18934853
nazbi <nazbi@86bc294b-834c-4f86-a8c2-b9af99263fca>
2022-02-12 21:50:22 +0000

    Implemented Aux Controls

--- src/main/include/Controls.h
+++ src/main/include/Controls.h
@@ -16,14 +16,30 @@ public:
 
     void process() override;
 
+
 private:
+
+    void doDrive();
+    void doAux();
+    void doSwitchPanel();
     Drive* drive;
     GamEpiece* gamEpiece;
     Hang* hang;
     bool cameraWasToggled = false;
     bool offsetsWereConfigured = false;
 
-    frc::Joystick controllerDriver{0};
+    frc::Joystick controllerDrive{0};
     frc::Joystick controllerAux{1};
+    frc::Joystick switchPanel{2};
+
+//switchPanel Variables
+    bool hangActive = false;
+    bool hangManual = false;
+    bool gamePieceManual = false;
+
+//Normal Aux Variables
+    Shooter::ShooterMode lastPressedMode = Shooter::TARMAC_LINE;
+//Manual Aux Variables
+    int lastDPadValue = -1
     // Something here...
 };
